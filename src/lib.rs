258a32cead11e903fb0fe87d5c144a525eca590a
Andrew Graham-Yooll <andrewgy8@gmail.com>
2018-11-03 16:09:57 +0100

    Check that packages can theoretically be packed in bin

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@ struct Bin {
     contents: Vec<u32>
 }
 
-fn pack_bins(mut packages: Vec<u32>) -> Vec<Bin>{
+fn pack_bins(bin_size: u32, mut packages: Vec<u32>) -> Vec<Bin>{
 	let mut bins: Vec<Bin> = vec![];
 	packages.sort_by(|a, b| b.cmp(a));
 
+	if &bin_size < packages.iter().max().unwrap() {
+		panic!("Bin size must be larger than max value.");
+	}
+	
 	while packages.len() > 0 {
 
 		let mut initial_bin = Bin{contents: vec![]};
@@ -14,7 +18,7 @@ fn pack_bins(mut packages: Vec<u32>) -> Vec<Bin>{
 		for package in packages.clone() {
 
 			let res: u32 = initial_bin.contents.iter().sum();
-			if (res + package) <= 6 {
+			if (res + package) <= bin_size {
 				initial_bin.contents.push(package);
 				
 				let pos = packages.iter().position(|&x| x == package).unwrap();
@@ -23,7 +27,7 @@ fn pack_bins(mut packages: Vec<u32>) -> Vec<Bin>{
 			}
 
 			let after_sum: u32 = initial_bin.contents.iter().sum();
-			if (after_sum == 6) | (packages.len() == 0) {
+			if (after_sum == bin_size) | (packages.len() == 0) {
 				bins.push(initial_bin);
 
 				break;
@@ -52,7 +56,7 @@ mod tests {
         	Bin{contents:vec![2, 1, 1, 1, 1]}
         ];
 
-        let packed_bins = pack_bins(rectangles);
+        let packed_bins = pack_bins(6, rectangles);
         
         assert_eq!(packed_bins, expected);
     }
@@ -67,8 +71,30 @@ mod tests {
         	Bin{contents:vec![1]}
         ];
 
-        let packed_bins = pack_bins(rectangles);
+        let packed_bins = pack_bins(6, rectangles);
         
         assert_eq!(packed_bins, expected);
+    }
+
+    #[test]
+    fn returns_bin_size_when_last_bin_not_perfect_size() {
+        let rectangles = vec![6, 4, 2, 2, 1, 1, 1, 1, 1];
+        let expected = vec![
+        	Bin{contents:vec![6, 2]}, 
+        	Bin{contents:vec![4, 2, 1, 1]}, 
+        	Bin{contents:vec![1, 1, 1]}
+        ];
+
+        let packed_bins = pack_bins(8, rectangles);
+        
+        assert_eq!(packed_bins, expected);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panics_when_bin_size_is_smaller_than_largest_given_size() {
+        let rectangles = vec![6, 4, 2, 2, 1, 1, 1, 1, 1];
+
+        pack_bins(2, rectangles);
     }
 }
